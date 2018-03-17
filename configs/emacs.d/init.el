0040300aa17ae643d0362517e0993847009c9cbb
Jed Clinger <jed.clinger@gmail.com>
2018-03-16 20:52:56 -0700

    Configure package, yasnippet, and magit

---
+++ configs/emacs.d/init.el
@@ -0,0 +1,63 @@
+;;; init.el --- My Emacs configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Don't hardcode .emacs.d location
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
+
+;; Configure package.el
+(setq package-enable-at-startup nil)
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+
+;; Configure load path
+(defconst my/lisp-dir (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path my/lisp-dir)
+(let ((default-directory my/lisp-dir))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+(add-to-list 'exec-path "/usr/local/bin")
+
+;; Install use-package
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; Essentials
+
+;; Configure packages
+
+(use-package init-look-and-feel)
+
+(use-package paredit
+  :ensure t
+  :init
+  (progn
+    (add-hook 'emacs-lisp-mode-hook 'paredit-mode)))
+
+(use-package yasnippet
+  :ensure t
+  :defer 10
+  :init (yas-global-mode))
+
+(use-package magit
+  :ensure t
+  :defer 5
+  :bind (("C-c g" . magit-status)))
+
+;;; init.el ends here
---
+++ configs/emacs.d/lisp/init-look-and-feel.el
@@ -0,0 +1,10 @@
+;;; init-look-and-feel.el --- Configure visual aesthetic -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+
+(provide 'init-look-and-feel)
+;;; init-look-and-feel.el ends here
+
+
