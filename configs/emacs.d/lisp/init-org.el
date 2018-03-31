09325baa4fed39f8933c17d5a4d5465ba4878546
Jed Clinger <jed.clinger@gmail.com>
2018-03-31 11:18:32 -0700

    No spaces after org headings

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -4,16 +4,20 @@
 
 ;; Hope and pray that this brings in org 9
 ;; org-version
-
 (use-package org
   :ensure t
   :defer t
   :init
   (define-obsolete-function-alias 'org-define-error 'define-error)
+
+  (setq org-blank-before-new-entry '((heading . nil)
+                                     (plain-list-item . nil)))
+
   :config
   (use-package org-bullets :ensure t)
   (use-package ox-reveal :ensure t)
   (use-package ox-gfm :ensure t)
+
   (add-hook 'org-mode-hook
             (lambda () (org-bullets-mode 1)))
   (org-babel-do-load-languages 'org-babel-load-languages
