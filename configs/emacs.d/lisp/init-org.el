de7b78cf94c420776e42ac5ed907f1d007cb8850
Jed Clinger <jed.clinger@gmail.com>
2018-03-31 15:34:36 -0700

    Configure org-capture

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -7,16 +7,28 @@
 (use-package org
   :ensure t
   :defer t
+  :bind (("C-c c" . org-capture))
   :init
   (define-obsolete-function-alias 'org-define-error 'define-error)
 
   (setq org-blank-before-new-entry '((heading . nil)
                                      (plain-list-item . nil)))
 
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "WIP(i!)" "|" "DONE(d!)" "CANCELED(c@)")))
   :config
   (use-package org-bullets :ensure t)
   (use-package ox-reveal :ensure t)
   (use-package ox-gfm :ensure t)
+  (use-package org-capture
+    :init
+    (setq org-default-notes-file "~/.emacs.d/notes.org")
+    (setq org-capture-templates
+          '(("t"
+             "Todo list item"
+             entry
+             (file+headline org-default-notes-file "Tasks")
+             "* TODO %?\n  %i\n Context: [[%l][%f]]"))))
 
   (add-hook 'org-mode-hook
             (lambda () (org-bullets-mode 1)))
