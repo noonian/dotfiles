ffaa6beb120f5e18954aa72c4cf66eff00127605
Jed Clinger <jed.clinger@gmail.com>
2018-04-07 15:19:34 -0700

    Fix smartparen hooks for Clojure

--- configs/emacs.d/lisp/init-clojure.el
+++ configs/emacs.d/lisp/init-clojure.el
@@ -6,18 +6,23 @@
   :ensure t
   :defer 30
   :config
+  (add-hook 'cljure-mode-hook #'smartparens-strict-mode)
+  (add-hook 'cljurescript-mode-hook #'smartparens-strict-mode)
+
   (use-package cider
     :ensure t
     :diminish t
     :commands cider-mode
-    :config (add-hook 'cider-repl-mode-hook (lambda () (paredit-mode 1)))
+    :config
+    (add-hook 'cider-repl-mode-hook
+              (lambda ()
+                (smartparens-strict-mode 1)
+                (company-mode 1)))
     :bind (("C-c M-j" . cider-jack-in)
            ("C-c M-c" . cider-connect)
            ("C-c v" . cider-eval-buffer)
            ("s-t" . cider-test-run-ns-tests)))
 
-  (add-hook 'cljure-mode-hook #'smartparens-strict-mode)
-  (add-hook 'cljurescript-mode-hook #'smartparens-strict-mode)
 )
 
 (provide 'init-clojure)
